88892ba3ee47c84b43d45b2944b78e7faf91a9cf
JaredMalan7 <jared.malan7@gmail.com>
2022-03-30 21:48:15 -0600

    update on collide

--- game/collide.py
+++ game/collide.py
@@ -1,3 +1,11 @@
+
+"""
+this function handles collisions between lasers and the ships using an overlap function.
+the laser overlaps the picture where there is an empty area on the png file and hits the
+actual ship.
+"""
+
+
 def collide(obj1, obj2):
     offset_x = obj2.x - obj1.x
     offset_y = obj2.y - obj1.y
